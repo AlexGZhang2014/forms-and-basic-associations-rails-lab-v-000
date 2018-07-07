bd009966b26043b673c28b8f6902fea1ee028e7a
Alex Zhang <alexgzhang92@gmail.com>
2018-07-07 23:56:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(notes)
     notes.each do |content|
       if content != ""
-        note = Note.find_by(content: content)
+        note = Note.new(content: content)
         self.notes << note
         #self.notes.build(content: content)
       end
